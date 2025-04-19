9a52a2c281dfa444cbc103ea813568b5c8aeae49
Aviral Dixit <aviraldxt345@gmail.com>
2025-04-19 20:00:27 +0530

    AlmostCmpltd

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,36 +1,93 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from './TodoItems'
+
+
 const Todo = () => {
-  return (
-    <div className='bg-white place-self-center p-7 w-[90%] max-w-md flex flex-col min-h-[550px] rounded-xl'>
-     
-  {/* ........Title....... */}
-   
-   <div className='flex items-center mt-7 gap-2'>
-     <img  className='w-10' src={todo_icon} alt="todo-logo" />
-     <h1 className='text-3xl font-semibold'>To-Do List</h1>
-  </div>
 
-   {/* ........input-box....... */}
-
-    <div className='flex items-center my-7 bg-gray-300 rounded-full'>
-        <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text"  placeholder='Add your task'/>
-        <button className='border-none rounded-full bg-orange-500 w-32 h-14 text-white text-lg cursor-pointer font-medium'>Add +</button>
-    </div>
-
-   
-    {/* ........toDo-list....... */}
-
-    <div>
-        <TodoItems text="learn coding"/>
-       
-    </div>
+    const [todolist, setTodolist] = useState([]);
 
 
+    const inputref = useRef()
 
-    </div>
-  )
+    const getInput = () => {
+        const inputext = inputref.current.value.trim();
+
+        if (inputext === "") {
+            return null;
+        }
+
+        const newTodo = {
+            id: Date.now(),
+            text: inputext,
+            isComplete: false
+        }
+
+        setTodolist((prev) => [...prev, newTodo]);
+        inputref.current.value = "";
+    }
+
+    const deleteTodo = (id) => {
+
+        setTodolist((prevTodos) => {
+            return prevTodos.filter((todo) => todo.id !== id)
+        })
+    }
+
+
+    const toggle = (id) => {
+
+        setTodolist((prevTodos) => {
+            return prevTodos.map((todo) => {
+                if (todo.id == id) {
+                    return { ...todo, isComplete: !todo.isComplete }
+                }
+
+                return todo;
+            })
+        })
+    }
+
+
+    //  useEffect(()=>{
+    //      console.log(todolist);
+         
+    //  },[todolist])
+    
+
+
+    return (
+        <div className='bg-white place-self-center p-7 w-[90%] max-w-md flex flex-col min-h-[550px] rounded-xl'>
+
+            {/* ........Title....... */}
+
+            <div className='flex items-center mt-7 gap-2'>
+                <img className='w-10' src={todo_icon} alt="todo-logo" />
+                <h1 className='text-3xl font-semibold'>To-Do List</h1>
+            </div>
+
+            {/* ........input-box....... */}
+
+            <div className='flex items-center my-7 bg-gray-300 rounded-full'>
+                <input ref={inputref} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
+                <button onClick={getInput} className='border-none rounded-full bg-orange-500 w-32 h-14 text-white text-lg cursor-pointer font-medium'>Add +</button>
+            </div>
+
+
+            {/* ........toDo-list....... */}
+
+            <div>
+
+                {todolist.map((item, index) => {
+                    return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle}/>
+                })}
+
+            </div>
+
+
+
+        </div>
+    )
 }
 
 export default Todo
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -3,16 +3,18 @@
  import not_tick from '../assets/not_tick.png'
  import delete_icon from '../assets/delete.png'
 
- const TodoItems = ({text}) => {
+ const TodoItems = ({text ,id, isComplete,deleteTodo , toggle}) => {
+
+
    return (
      <div className='flex items-center my-3 gap-2 '>
   
-        <div className='flex flex-1 items-center cursor-pointer '>
-              <img className='w-7' src= {tick} alt="tick-icon" />
-              <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
+        <div onClick={()=>toggle(id)} className='flex flex-1 items-center cursor-pointer '>
+              <img className='w-7' src= { isComplete ? tick : not_tick} alt="tick-icon" />
+              <p className={`text-slate-700 ml-4 text-[17px] ${ isComplete? "line-through" : ""}`}>{text}</p>
         </div>
          
-         <img src={delete_icon} alt="dlt-icon" className='w-3.5 cursor-pointer' />
+         <img onClick={()=> deleteTodo(id)} src={delete_icon} alt="dlt-icon" className='w-3.5 cursor-pointer' />
 
      </div>
    )
